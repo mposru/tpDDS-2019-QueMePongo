32370ab813b965397d7a2737389410967d41132f
alexisdona1983 <alexis.dona@gmail.com>
2019-04-23 22:25:01 -0300

    Separación de Test para Prenda y Borrador

--- src/test/java/PrendaTest.java
+++ src/test/java/PrendaTest.java
@@ -7,45 +7,6 @@ import org.junit.rules.ExpectedException;
 
 public class PrendaTest {
 
-    private Prenda prenda;
-    private TipoDePrenda tipoDePrenda;
-    private Material material;
-    private Color colorPrimario;
-    private Color colorSecundario;
-    private Color colorSecundarioIgualPrimario;
-    private Color otroColorSecundario;
-    private Guardarropa guardarropa = new Guardarropa();
-    private Borrador borradorZapatillas
-
-    @Before
-    this.tipoDePrenda = tipoDePrenda.ZAPATO;
-    this.colorPrimario = new Color(20,20,30);
-    this.colorSecundarioIgualPrimario = new Color(20,20,30);
-    this.otroColorSecundario = new Color(25,40,88);
-
-    public void iniciar() {
-        borradorZapatillas = new Borrador();
-        borradorZapatillas.definirColorPrimario(colorPrimario);
-    }
-    @Test
-    public void setearColorSecundarioIgualAlPrimario(){
-        try {
-            this.borradorZapatillas.definirColorSecundario(colorSecundarioIgualPrimario);
-        }
-        catch (ColorSecundarioException exception){
-            System.out.print(exception.getMessage());
-        }
-    }
-
-    @Test
-    public void deberiaHaberTipoDePrendaAlCrearPrenda() {
-        exception.expect(NullPointerException.class);
-        exception.expectMessage("el tipo de prenda es obligatorio");
-
-        material = Material.ALGODON;
-        color = new Color(100,100,100);
-        prenda = new Prenda(tipoDePrenda, material, color);
-    }
 
 
     // Hay que agregar mas Test y retocar los que estan
