873f6342d313cc8e7b35991003e543fc75c2e988
Daiana Szwimer <daiana.szwimer@prosoftwarefactory.com.ar>
2019-04-23 22:50:14 -0300

    agregue atributos a usuario y funcion para agregar guardarropa

--- src/main/java/domain/Usuario.java
+++ src/main/java/domain/Usuario.java
@@ -3,5 +3,10 @@ package domain;
 import java.util.Set;
 
 public class Usuario {
+    private String nombre, apellido, mail;
     private Set<Guardarropa> guardarropas;
+
+    public void agregarGuardarropa(Guardarropa guardarropa) {
+        this.guardarropas.add(guardarropa);
+    }
 }
