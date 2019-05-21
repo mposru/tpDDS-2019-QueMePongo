26a958bd069b5139745c86d17c358410ddfa81b9
mposru <mposru@est.frba.utn.edu.ar>
2019-05-21 17:23:16 -0300

    Estructura Json falta parsear

--- src/main/java/domain/AccuWeather.java
+++ src/main/java/domain/AccuWeather.java
@@ -1,10 +1,24 @@
 package domain;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import javax.ws.rs.core.MediaType;
+
 
 public class AccuWeather {
-    Client client = ClientBuilder.newClient();
-    WebTarget webTarget = client.target("http://localhost:8082/spring-jersey");
+    private Client client;
+    private static final String API_ACCUWEATHER = "https://dataservice.accuweather.com/forecasts/v1/daily/1day/7894?apikey=QZYTHbRTv93BEQmByL07F0ssLgYyNhYH&language=es-ar";
+
+    //Inicializacion del cliente
+    public AccuWeather() {
+        this.client = Client.create();
+    }
+
+    public ClientResponse getJsonClima(){
+        WebResource recurso = this.client.resource(API_ACCUWEATHER);
+        WebResource.Builder builder = recurso.accept(MediaType.APPLICATION_JSON);
+        ClientResponse response = builder.get(ClientResponse.class);
+        return response;
+    }
 }
---
+++ src/main/java/domain/Clima.java
@@ -0,0 +1,13 @@
+package domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Clima {
+
+    private Integer maxima;
+    private Integer minima;
+
+    @JsonProperty("id_card")
+    private long idCard;
+
+}
