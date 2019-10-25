9ce242ca4f57406422166714f5d0e89700aca16c
mposru <mposru@est.frba.utn.edu.ar>
2019-10-25 17:32:54 -0300

    Little fixes

--- src/main/java/controller/ControllerGuardarropas.java
+++ src/main/java/controller/ControllerGuardarropas.java
@@ -33,19 +33,32 @@ public class ControllerGuardarropas {
         Set<Usuario> usuariosLista = new HashSet<>();
         usuariosLista.add(usuario);
 
-        Guardarropa guardarropa1 = new Guardarropa("Guardarropa1",new HashSet<>(),new Premium());
-        Guardarropa guardarropa2 = new Guardarropa("Guardarropa2",new HashSet<>(),new Gratuito(2));
+        Guardarropa guardarropa1 = new Guardarropa("GuardarropaInvierno",new HashSet<>(),new Premium());
+        Guardarropa guardarropa2 = new Guardarropa("GuardarropaVerano",new HashSet<>(),new Gratuito(10));
 
 
-        Prenda pantalon = new Prenda("Pantalon",TipoDePrenda.PANTALON, Material.ALGODON, new Color(1,2,3), null, Trama.CUADROS, guardarropa1, true);
         Prenda zapatos = new Prenda("Zapatos",TipoDePrenda.ZAPATO, Material.CUERO, new Color(1,2,3), null, Trama.LISA, guardarropa1, true);
-        Prenda shortDeJean = new Prenda("ShortDeJean", TipoDePrenda.SHORT, Material.JEAN, new Color(1,2,3), null, Trama.LISA, guardarropa1, false);
+        Prenda botasDeNieve = new Prenda("BotasDeNieve",TipoDePrenda.BOTAS_NIEVE, Material.ALGODON, new Color(1,2,3), null, Trama.CUADROS, guardarropa1, false);
+        Prenda buzo = new Prenda("Buzo",TipoDePrenda.BUZO, Material.ALGODON, new Color(1,2,3), null, Trama.CUADROS, guardarropa1, false);
+        Prenda pantalonPolar = new Prenda("Pantalon Polar",TipoDePrenda.PANTALON, Material.ALGODON, new Color(1,2,3), null, Trama.CUADROS, guardarropa1, false);
+        Prenda gorro = new Prenda("Gorro lana",TipoDePrenda.GORRO, Material.LANA, new Color(1,2,3), null, Trama.LISA, guardarropa1, false);
+
+
+        Prenda musculosa = new Prenda("Musculosa",TipoDePrenda.MUSCULOSA, Material.ALGODON, new Color(1,2,3), null, Trama.LISA, guardarropa2,  false);
+        Prenda shortDeJean = new Prenda("ShortDeJean", TipoDePrenda.SHORT, Material.JEAN, new Color(1,2,3), null, Trama.LISA, guardarropa2, false);
+        Prenda crocs = new Prenda("Crocs",TipoDePrenda.CROCS, Material.GOMA, new Color(1,2,3), null, Trama.CUADROS, guardarropa2, true);
         Prenda pollera = new Prenda("Pollera", TipoDePrenda.POLLERA, Material.ALGODON, new Color(1,2,3), null, Trama.LISA, guardarropa2, false);
         Prenda pañuelo = new Prenda("Pañuelo",TipoDePrenda.PANUELO, Material.ALGODON, new Color(1,2,3), null, Trama.LISA, guardarropa2, false);
 
-        guardarropa1.guardarPrenda(pantalon);
         guardarropa1.guardarPrenda(zapatos);
-        guardarropa1.guardarPrenda(shortDeJean);
+        guardarropa1.guardarPrenda(botasDeNieve);
+        guardarropa1.guardarPrenda(buzo);
+        guardarropa1.guardarPrenda(pantalonPolar);
+        guardarropa1.guardarPrenda(gorro);
+
+        guardarropa2.guardarPrenda(shortDeJean);
+        guardarropa2.guardarPrenda(musculosa);
+        guardarropa2.guardarPrenda(crocs);
         guardarropa2.guardarPrenda(pollera);
         guardarropa2.guardarPrenda(pañuelo);
 
