c11b24b1e7dbeaea8bc6de8cadc8dd4c63fd3a64
Alexis Do Nascimento <alexis.dona@gmail.com>
2019-11-29 19:53:21 -0300

    fix de alta de prenda(parcial)

---
+++ src/main/java/domain/RepositorioTipoDePrendas.java
@@ -0,0 +1,33 @@
+package domain;
+
+import domain.prenda.TipoDePrenda;
+import domain.usuario.Evento;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioTipoDePrendas {
+    private static RepositorioTipoDePrendas instanceOfRepositorioTipoDePrendas;
+    private List<TipoDePrenda> tipoDePrendas = new ArrayList<>();
+    public static RepositorioTipoDePrendas getInstance() {
+        if(instanceOfRepositorioTipoDePrendas==null) {
+            instanceOfRepositorioTipoDePrendas = new RepositorioTipoDePrendas();
+        }
+        return instanceOfRepositorioTipoDePrendas;
+    }
+    public List<TipoDePrenda> getTipoDePrendas () {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("quemepongo");
+        EntityManager manager = emf.createEntityManager();
+        Query query = manager.createQuery("select tp from TipoDePrenda tp"); //levantamos la lista de tipo de prendas de la base de datos
+        tipoDePrendas = query.getResultList();
+        manager.close();
+        emf.close();
+        return tipoDePrendas;
+    }
+
+
+}
