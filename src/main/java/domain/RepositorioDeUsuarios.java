5fabc6072a377cb394c6251279a31e86d37850cc
Alexis Do Nascimento <alexis.dona@gmail.com>
2019-12-15 18:25:43 -0300

    me la mandé con el persistenceUnitName y lo corrijo

--- src/main/java/domain/RepositorioDeUsuarios.java
+++ src/main/java/domain/RepositorioDeUsuarios.java
@@ -24,7 +24,7 @@ public class RepositorioDeUsuarios {
     }
 
     public RepositorioDeUsuarios() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("quemepongo");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("dxffzlciern157vi");
         EntityManager manager = emf.createEntityManager();
         Query query = manager.createQuery("select a from Usuario a"); //levantamos la lista de usuarios de la BBDD
         usuarios = query.getResultList();
